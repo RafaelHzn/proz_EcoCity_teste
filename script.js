660ffc40dd2a338e5021046cab6811a0783c3b1d
Rafael Ferreira <rafael.bemestar@gmail.com>
2024-02-29 19:08:33 -0300

    update

--- script.js
+++ script.js
@@ -111,57 +111,57 @@ mostrarPopup(telInput, telLabel);
 mostrarPopup(messageInput, messageLabel);
 
 // --- Validação de nome
-nomeInput.addEventListener("change", (e)=> {
-    let valor = e.target.value;
-    if (valor.length < 3) {
-        //Estilo dinâmico para nome incorreto
-        nomeHelper.innerText = "O nome precisa ter 3 ou mais caracteres.";
-        estilizarInputIncorreto(nomeInput, nomeHelper);
-    } else {
-        //Estilo dinâmico para nome correto
-        estilizarInputCorreto(nomeInput, nomeHelper);
-    }
-})
+// nomeInput.addEventListener("change", (e)=> {
+//     let valor = e.target.value;
+//     if (valor.length < 3) {
+//         //Estilo dinâmico para nome incorreto
+//         nomeHelper.innerText = "O nome precisa ter 3 ou mais caracteres.";
+//         estilizarInputIncorreto(nomeInput, nomeHelper);
+//     } else {
+//         //Estilo dinâmico para nome correto
+//         estilizarInputCorreto(nomeInput, nomeHelper);
+//     }
+// })
 
-// --- Validação de e-mail
-emailInput.addEventListener("change", (e)=> {
-    let valor = e.target.value;
-    if (valor.includes("@") && valor.includes(".com")) {
-        //Estilo dinâmico para e-mail válido
-        estilizarInputCorreto(emailInput, emailHelper);   
-    } else {
-        //Estilo dinâmico para e-mail inválido
-        emailHelper.innerText = "Insira um e-mail válido.";
-        estilizarInputIncorreto(emailInput, emailHelper); 
-    }
-})
+// // --- Validação de e-mail
+// emailInput.addEventListener("change", (e)=> {
+//     let valor = e.target.value;
+//     if (valor.includes("@") && valor.includes(".com")) {
+//         //Estilo dinâmico para e-mail válido
+//         estilizarInputCorreto(emailInput, emailHelper);   
+//     } else {
+//         //Estilo dinâmico para e-mail inválido
+//         emailHelper.innerText = "Insira um e-mail válido.";
+//         estilizarInputIncorreto(emailInput, emailHelper); 
+//     }
+// })
 
-// --- Validação de telefone
-telInput.addEventListener("change", (e)=> {
-    let valor = e.target.value;
-    if (valor < 18) {
-        //Estilo dinâmico para tel abaixo de 18 anos
-        telHelper.innerText = "Você tem menos de 18 anos.";
-        estilizarInputIncorreto(telInput, telHelper);
-    } else {
-        //Estilo dinâmico para tel acima de 18 anos
-        estilizarInputCorreto(telInput, telHelper);
-    }
-})
+// // --- Validação de telefone
+// telInput.addEventListener("change", (e)=> {
+//     let valor = e.target.value;
+//     if (valor < 18) {
+//         //Estilo dinâmico para tel abaixo de 18 anos
+//         telHelper.innerText = "Você tem menos de 18 anos.";
+//         estilizarInputIncorreto(telInput, telHelper);
+//     } else {
+//         //Estilo dinâmico para tel acima de 18 anos
+//         estilizarInputCorreto(telInput, telHelper);
+//     }
+// })
 
-// --- Validação de mensagem
-messageInput.addEventListener("blur", (e)=> {
-    let valor = e.target.value;
-    if (valor == "") {
-        //Estilo dinâmico para message incorreta
-        messageHelper.innerText = "O campo não pode estar vazio.";
-        estilizarInputIncorreto(messageInput, messageHelper);
-    } else {
-        //Estilo dinâmico para message correta
-        messageHelper.innerText = "message correta";
-        estilizarInputCorreto(messageInput, messageHelper);
-    }
-})
+// // --- Validação de mensagem
+// messageInput.addEventListener("blur", (e)=> {
+//     let valor = e.target.value;
+//     if (valor == "") {
+//         //Estilo dinâmico para message incorreta
+//         messageHelper.innerText = "O campo não pode estar vazio.";
+//         estilizarInputIncorreto(messageInput, messageHelper);
+//     } else {
+//         //Estilo dinâmico para message correta
+//         messageHelper.innerText = "message correta";
+//         estilizarInputCorreto(messageInput, messageHelper);
+//     }
+// })
 
 /* -------------------------------------------------------------- */
 
